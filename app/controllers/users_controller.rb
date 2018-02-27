996da7c6234df80445537279bad7e47b38d02d2e
dflomo <dflomo@gmail.com>
2018-02-27 11:20:45 -0800

    Implement basic login: secure pass, gravatar, etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,16 +11,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = "Derek's App: Login Successful!"
       redirect_to @user
     else
       render 'new'
     end
   end
 
-  def user_params
-    params.require(:user).permit(:name, :email, 
-                                 :password, :password_confirmation)
-  end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, 
+                                  :password, :password_confirmation)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
   
   get '/home', to: 'static_pages#home'
@@ -8,5 +9,11 @@ Rails.application.routes.draw do
   
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
+
 end
