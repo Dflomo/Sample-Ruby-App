22605d00ac450a55ea943823058f2b052315d20c
dflomo <dflomo@gmail.com>
2018-04-04 16:15:17 -0700

    Changed config/production.rb host variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'pacific-tor-39359.herokuapp.com'
+  host = 'https://pacific-tor-39359.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings = {
     :user_name            => ENV['SENDGRID_USERNAME'],
