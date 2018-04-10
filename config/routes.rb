1e0d66850cfcdf6f24436572f3f88ea4fc75c583
dflomo <dflomo@gmail.com>
2018-04-09 17:08:50 -0700

    Add password reset email

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-
   root 'static_pages#home'
   
   get '/home', to: 'static_pages#home'
+
   get '/help', to: 'static_pages#help'
+
   get '/about', to: 'static_pages#about'
+  
   get '/contact', to: 'static_pages#contact'
   
   get '/signup', to: 'users#new'
@@ -16,4 +18,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
